415c1d9b109c8358c215279b16f87d3fb3852613
Nicolly Ant�nia de Oliveira Souza <47819795808@>
2024-12-13 15:45:30 -0300

    primeiro commit

---
+++ integrador_frontend-main/scripts/database.js
@@ -0,0 +1,288 @@
+export const database = [ /* "Banco de dados" de produtos comercializados no site */
+    {
+        codigoProduto:1,
+        tituloProduto:"Legging Nike One Maternidade Feminina", 
+        preco: 220.00, 
+        descricao:"Treino & Academia",
+        imagemProduto: {
+            img1:"./images/products/PROD1.avif",
+            img2:"./images/products/PROD1.IMG2.avif",
+            img3:"./images/products/PROD1.IMG3.avif",
+            img4:"./images/products/PROD1.IMG4.avif",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:2,
+        tituloProduto:"Calça Jordan Knit Track Feminina", 
+        preco: 300.00, 
+        descricao:"Casual",
+        imagemProduto: {
+            img1:"./images/products/PROD2.avif",
+            img2:"./images/products/PROD2.IMG2.avif",
+            img3:"./images/products/PROD2.IMG3.avif",
+            img4:"./images/products/PROD2.IMG4.avif",
+    },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:3,
+        tituloProduto:"Calça Nike Sportswear Trend Woven Feminina", 
+        preco: 300.00, 
+        descricao:"Casual",
+        imagemProduto: {
+            img1:"./images/products/PROD3.avif",
+            img2:"./images/products/PROD3.IMG2.avif",
+            img3:"./images/products/PROD3.IMG3.avif",
+            img4:"./images/products/PROD3.IMG4.avif",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto:"Novidades",
+        exibirHome:true
+    },
+    {
+        codigoProduto:4,
+        tituloProduto:"Tênis Nike Pegasus 39 Shield Masculino", 
+        preco: 300.00, 
+        descricao:"Corrida",
+        imagemProduto: {
+            img1:"./images/products/PROD4.avif",
+            img2:"./images/products/PROD4.IMG2.avif",
+            img3:"./images/products/PROD4.IMG3.avif",
+            img4:"./images/products/PROD4.IMG4.",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:5,
+        tituloProduto:"Tênis Nike Vomero 16 Masculino", 
+        preco: 300.00, 
+        descricao:"Corrida",
+        imagemProduto: {
+            img1:"./images/products/PROD5.avif",
+            img2:"./images/products/PROD5.IMG2.avif",
+            img3:"./images/products/PROD5.IMG3.avif",
+            img4:"./images/products/PROD5.IMG4.",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:6,
+        tituloProduto:"Mochila Nike Sportswear Futura 365 Feminina", 
+        preco: 300.00, 
+        descricao:"Casual",
+        imagemProduto: {
+            img1:"./images/products/PROD6.avif",
+            img2:"./images/products/PROD6.IMG2.avif",
+            img3:"./images/products/PROD6.IMG3.avif",
+            img4:"./images/products/PROD6.IMG4.",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:7,
+        tituloProduto:"Caneleira Nike J Guard Unissex", 
+        preco: 300.00, 
+        descricao:"Futebol",
+        imagemProduto: {
+            img1:"./images/products/PROD7.avif",
+            img2:"./images/products/PROD7.IMG2.avif",
+            img3:"./images/products/PROD7.IMG3.avif",
+            img4:"./images/products/PROD7.IMG4.",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:8,
+        tituloProduto:"Bola Nike Futsal Maestro Unissex", 
+        preco: 300.00, 
+        descricao:"Futebol.",
+        imagemProduto: {
+            img1:"./images/products/PROD8.avif",
+            img2:"./images/products/PROD8.IMG2.avif",
+            img3:"./images/products/PROD8.IMG3.avif",
+            img4:"./images/products/PROD8.IMG4.",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:9,
+        tituloProduto:"Bola Nike Pitch Futebol", 
+        preco: 300.00, 
+        descricao:"Futebol",
+        imagemProduto: {
+            img1:"./images/products/PROD9.avif",
+            img2:"./images/products/PROD9.IMG2.avif",
+            img3:"./images/products/PROD9.IMG3.avif",
+            img4:"./images/products/PROD9.IMG4.",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:10,
+        tituloProduto:"Meia Nike Everyday Plus Cushioned (3 Pares) Masculina", 
+        preco: 220.00, 
+        descricao:"Treino & Academia",
+        imagemProduto: {
+            img1:"./images/products/PROD10.avif",
+            img2:"./images/products/PROD10.IMG2.avif",
+            img3:"./images/products/PROD10.IMG3.avif",
+            img4:"./images/products/PROD10.IMG4.avif",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+    {
+        codigoProduto:11,
+        tituloProduto:"Bolsa Nike Gym Club Feminina", 
+        preco: 300.00, 
+        descricao:"Treino & Academia",
+        imagemProduto: {
+            img1:"./images/products/PROD11.avif",
+            img2:"./images/products/PROD11.IMG2.avif",
+            img3:"./images/products/PROD11.IMG3.avif",
+            img4:"./images/products/PROD11.IMG4.avif",
+    },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+    {
+        codigoProduto:12,
+        tituloProduto:"Luvas de Goleiro Nike Match Infantil", 
+        preco: 300.00, 
+        descricao:"Pré-Adolescentes / Futebol",
+        imagemProduto: {
+            img1:"./images/products/PROD12.avif",
+            img2:"./images/products/PROD12.IMG2.avif",
+            img3:"./images/products/PROD12.IMG3.avif",
+            img4:"./images/products/PROD12.IMG4.avif",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome:true
+    },
+    {
+        codigoProduto:13,
+        tituloProduto:"Jaqueta Nike Sportswear Woven Feminina", 
+        preco: 300.00, 
+        descricao:"Casual",
+        imagemProduto: {
+            img1:"./images/products/PROD13.avif",
+            img2:"./images/products/PROD13.IMG2.avif",
+            img3:"./images/products/PROD13.IMG3.avif",
+            img4:"./images/products/PROD13.IMG4.",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+    {
+        codigoProduto:14,
+        tituloProduto:"Top Nike Swoosh Feminino", 
+        preco: 300.00, 
+        descricao:"Suporte Médio",
+        imagemProduto: {
+            img1:"./images/products/PROD14.avif",
+            img2:"./images/products/PROD14.IMG2.avif",
+            img3:"./images/products/PROD14.IMG3.avif",
+            img4:"./images/products/PROD14.IMG4.",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+    {
+        codigoProduto:15,
+        tituloProduto:"Camisa Polo Nike Sportswear Phoenix Fleece Feminina", 
+        preco: 300.00, 
+        descricao:"Casual",
+        imagemProduto: {
+            img1:"./images/products/PROD15.avif",
+            img2:"./images/products/PROD15.IMG2.avif",
+            img3:"./images/products/PROD15.IMG3.avif",
+            img4:"./images/products/PROD15.IMG4.",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+    {
+        codigoProduto:16,
+        tituloProduto:"Tênis Nike Free Metcon 6 Feminino", 
+        preco: 300.00, 
+        descricao:"Treino & Academia",
+        imagemProduto: {
+            img1:"./images/products/PROD16.avif",
+            img2:"./images/products/PROD16.IMG2.avif",
+            img3:"./images/products/PROD16.IMG3.avif",
+            img4:"./images/products/PROD16.IMG4.",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+    {
+        codigoProduto:17,
+        tituloProduto:"Camiseta Nike Dri-FIT Feminina", 
+        preco: 300.00, 
+        descricao:"Treino & Academia",
+        imagemProduto: {
+            img1:"./images/products/PROD17.avif",
+            img2:"./images/products/PROD17.IMG2.avif",
+            img3:"./images/products/PROD17.IMG3.avif",
+            img4:"./images/products/PROD17.IMG4.",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+    {
+        codigoProduto:18,
+        tituloProduto:"", 
+        preco: 300.00, 
+        descricao:"",
+        imagemProduto: {
+            img1:"./images/products/PROD18.avif",
+            img2:"./images/products/PROD18.IMG2.avif",
+            img3:"./images/products/PROD18.IMG3.avif",
+            img4:"./images/products/PROD18.IMG4.",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+]
